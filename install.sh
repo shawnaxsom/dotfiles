adbd3a0ace8d718a5b7e62945b4e7582c8932326
Shawn Axsom <shawn.axsom@rooksecurity.com>
2015-03-02 20:12:37 -0500

    Updated install shell to check for file existence and prompt user

--- install.sh
+++ install.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+prompted_to_delete=false
+
 create_symlink() {
-  if [[ -L "$2" ]]
-  then
-      echo "Removing symbolic link $2"
-      rm ~/.vimrc
+  if [ $prompted_to_delete=false ]; then
+    if [ ! -L "$2" ]; then
+      if [ -f "$2" ] || [ -d "2" ]; then 
+        read -p "Non-symbolic file already exists, okay to delete? (y/n)" yn
+
+        case $yn in
+          [Yy]* ) break;;
+          [Nn]* ) echo "Exiting..."; exit;;
+      * ) echo "Please answer yes or no.";;
+    esac
+
+    prompted_to_delete=true
   fi
+fi
+fi
+
+  if [ -f "$2" ] || [ -d "$2" ]; then
+    echo "Removing symbolic link $2"
+    rm $2
+  fi
+
+  echo "Creating symbolic link $2"
   ln -s $1 $2
 }
 
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/fish ~/.config/fish
+create_symlink    ~/.dotfiles/.vimrc     ~/.vimrc
+create_symlink    ~/.dotfiles/fish       ~/.config/fish
