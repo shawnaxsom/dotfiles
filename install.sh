d36b92a8cac5caca58dc712490747c66febe985b
Shawn Axsom <axs221@gmail.com>
2015-03-07 19:56:07 -0500

    Fixed bug in install script for deleting symbolic links

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ prompted_to_delete=false
 
 create_symlink() {
   if [ $prompted_to_delete=false ]; then
-    if [ ! -L "$2" ]; then
-      if [ -f "$2" ] || [ -d "$2" ]; then 
+    if [ ! -L $2 ]; then
+      if [ -f $2 ] || [ -d $2 ]; then 
         read -p "Non-symbolic file already exists, okay to delete? (y/n)" yn
 
         case $yn in
@@ -19,7 +19,7 @@ create_symlink() {
 fi
 fi
 
-  if [ -f "$2" ] || [ -d "$2" ]; then
+  if [ -L $2 ] || [ -f $2 ] || [ -d $2 ]; then
     echo "Removing symbolic link $2"
     rm -rf $2
   fi
@@ -28,8 +28,15 @@ fi
   ln -s $1 $2
 }
 
+
+if [ ! -d ~/.oh-my-fish ]; then
+  echo "Pulling down oh-my-fish"
+  git clone https://github.com/bpinto/oh-my-fish ~/.oh-my-fish
+fi
+
 create_symlink    ~/.dotfiles/.vimrc     ~/.vimrc
 create_symlink    ~/.dotfiles/fish       ~/.config/fish
-create_symlink    ~/.dotfiles/.zshrc     ~/.zshrc
+create_symlink    ~/.dotfiles/.fishrc    ~/.fishrc                        
 create_symlink    ~/.dotfiles/i3config   ~/.i3/config
 create_symlink    ~/.dotfiles/sift.vim   ~/.vim/colors/sift.vim
+
