00eecb6555b8419375566a3352f41b69dae33de1
Shawn Axsom <axs221@gmail.com>
2015-09-20 10:52:43 -0400

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -2,6 +2,6 @@
 
 echo "deb http://ppa.launchpad.net/fish-shell/release-2/ubuntu trusty main" > "/etc/apt/sources.list.d/fish-shell-release-2-trusty.list"
 
-apt-get update
+apt-get update --allow-unauthenticated
 
-apt-get install fish --force-yes
+apt-get install fish --force-yes --allow-unauthenticated
