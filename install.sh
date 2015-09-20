8a45e48b3b3073d827bd561c2a6c126ba6300fb2
Shawn Axsom <axs221@gmail.com>
2015-09-20 11:11:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -72,5 +72,5 @@ if [ ! -d ~/.vim/bundle ]; then
   install_cmake
   sudo apt-get install python-dev
   # ~/.vim/bundle/YouCompleteMe/install.sh
-  cd ~/.vim/bundle/tern_for_vim && npm install
+  # cd ~/.vim/bundle/tern_for_vim && npm install
 fi
